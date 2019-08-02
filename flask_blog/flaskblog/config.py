8610250ac174499d111a2a4e73a2b990b0db1241
samirboudissa1994 <samirboudissa1@msn.com>
2019-08-03 02:25:39 +0300

    commit

---
+++ flask_blog/flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '843aad7fe6d5d90144c168a94312f710'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "kurkku.mopo1@gmail.com"
+    MAIL_PASSWORD = "M4dm3n72"
